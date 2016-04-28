557436817d86ed0490feabd2ea7d156ea9fd88df
Henne123 <verify.wlc@gmail.com>
2016-04-28 20:25:54 +0200

    Create WorldleadcurrencyMain.java

---
+++ core/src/main/java/com/coinomi/core/coins/WorldleadcurrencyMain.java
@@ -0,0 +1,34 @@
+package com.coinomi.core.coins;
+
+import com.coinomi.core.coins.families.BitFamily;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class WorldleadcurrencyMain extends BitFamily {
+    private WorldleadcurrencyMain() {
+        id = "worldleadcurrency.main";
+
+        addressHeader = 0;
+        p2shHeader = 5;
+        acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
+        spendableCoinbaseDepth = 100;
+        dumpedPrivateKeyHeader = 128;
+
+        name = "Worldleadcurrency";
+        symbol = "WLC";
+        uriScheme = "worldleadcurrency";
+        bip44Index = 0;
+        unitExponent = 8;
+        feeValue = value(12000);
+        minNonDust = value(5460);
+        softDustLimit = value(1000000); // 0.01 WLC
+        softDustPolicy = SoftDustPolicy.AT_LEAST_BASE_FEE_IF_SOFT_DUST_TXO_PRESENT;
+        signedMessageHeader = toBytes("Bitcoin Signed Message:\n");
+    }
+
+    private static WorldleadcurrencyMain instance = new WorldleadcurrencyMain();
+    public static synchronized CoinType get() {
+        return instance;
+    }
+}
